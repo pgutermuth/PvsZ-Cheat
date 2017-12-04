8cd6283863a9679825495217ecdf938b4e5ea1ca
Paul " cragson <m.jenkins@outlook.de>
2017-12-04 02:53:32 +0100

    Create CPatch.h

---
+++ CPatch/CPatch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+
+#include <cstdint>
+
+class CPatch {
+
+public:
+
+	bool PatchByte( const byte &newByte, const std::uintptr_t &Address = 0 );
+
+};
+
+extern CPatch* g_pPatch;
