f719962e87bf43bc89a31b478dc5685e6992280b
Paul " cragson <m.jenkins@outlook.de>
2017-12-04 02:53:51 +0100

    Create CPatch.cpp

---
+++ CPatch/CPatch.cpp
@@ -0,0 +1,20 @@
+#include "CPatch.h"
+
+#include <assert.h>
+
+bool CPatch::PatchByte( const byte & newByte, const std::uintptr_t & Address ) {
+
+	assert( Address != 0 );
+
+	DWORD dwOldProtection{ 0 };
+
+	if ( VirtualProtect( reinterpret_cast< LPVOID >( Address ), sizeof( byte ), PAGE_EXECUTE_READWRITE, &dwOldProtection ) == 0 ) { return false; }
+
+	*(byte*)( Address ) = newByte;
+
+	if ( VirtualProtect( reinterpret_cast< LPVOID >( Address ), sizeof( byte ), dwOldProtection, &dwOldProtection ) == 0 ) { return false; }
+
+	return true;
+}
+
+CPatch* g_pPatch = new CPatch();
