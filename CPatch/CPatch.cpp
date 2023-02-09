3968f969a8f801ae61c38cd29f6043308ee0cd1b
Pascal Gutermuth <pascal.gutermuth@informatik.hs-fulda.de>
2023-02-09 19:00:00 +0100

    CPatch.cpp refactoring and Allman style

--- CPatch/CPatch.cpp
+++ CPatch/CPatch.cpp
@@ -2,15 +2,14 @@
 
 #include <assert.h>
 
-bool CPatch::PatchByte( const byte & newByte, const std::uintptr_t & Address ) {
+bool CPatch::PatchByte( const byte & newByte, const std::uintptr_t & Address ) 
+{
 	assert( Address != 0 );
 
 	DWORD dwOldProtection{ 0 };
-
 	if ( VirtualProtect( reinterpret_cast< LPVOID >( Address ), sizeof( byte ), PAGE_EXECUTE_READWRITE, &dwOldProtection ) == 0 ) { return false; }
 
 	*(byte*)( Address ) = newByte;
-
 	if ( VirtualProtect( reinterpret_cast< LPVOID >( Address ), sizeof( byte ), dwOldProtection, &dwOldProtection ) == 0 ) { return false; }
 
 	return true;
